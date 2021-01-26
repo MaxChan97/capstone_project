a20d0a66f52524d8d0693210ebd2c59f02eed32c
MaxChan97 <42763925+MaxChan97@users.noreply.github.com>
2021-01-26 12:16:59 +0800

    cleaned up merge conflict

--- frontend/src/components/ProfilePostCard.js
+++ frontend/src/components/ProfilePostCard.js
@@ -37,90 +37,80 @@ export default function ProfilePostCard() {
         <div class="card">
           <div class="card-body">
             <div class="post">
-              <i class="fa fa-ellipsis-v float-right" aria-hidden="true"></i>
-              <div class="user-block">
-                <img src={defaultDP} alt="User profile picture" />
-                <span class="username">
-                  <Link to="/">Teacher A</Link>
-                </span>
+              <div style={{ display: "flex", alignItems: "baseline" }}>
+                <div class="user-block">
+                  <img src={defaultDP} alt="User profile picture" />
+                  <span class="username">
+                    <Link to="/">Teacher A</Link>
+                  </span>
 
-                <div class="post">
-                  <div style={{ display: "flex", alignItems: "baseline" }}>
-                    <div class="user-block">
-                      <img src={defaultDP} alt="User profile picture" />
-                      <span class="username">
-                        <Link to="/">Teacher A</Link>
-                      </span>
-
-                      <span class="description">1 week ago</span>
-                    </div>
-                    <div style={{ textAlign: "right" }}>
-                      <IconButton
-                        aria-label="more"
-                        aria-controls="long-menu"
-                        aria-haspopup="true"
-                        onClick={handleClick}
+                  <span class="description">1 week ago</span>
+                </div>
+                <div style={{ textAlign: "right" }}>
+                  <IconButton
+                    aria-label="more"
+                    aria-controls="long-menu"
+                    aria-haspopup="true"
+                    onClick={handleClick}
+                  >
+                    <MoreVertIcon />
+                  </IconButton>
+                  <Menu
+                    id="long-menu"
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={open}
+                    onClose={handleClose}
+                    PaperProps={{
+                      style: {
+                        maxHeight: ITEM_HEIGHT * 4.5,
+                        width: "20ch",
+                      },
+                    }}
+                  >
+                    {options.map((option) => (
+                      <MenuItem
+                        key={option}
+                        selected={option === "Edit Post"}
+                        onClick={handleClose}
                       >
-                        <MoreVertIcon />
-                      </IconButton>
-                      <Menu
-                        id="long-menu"
-                        anchorEl={anchorEl}
-                        keepMounted
-                        open={open}
-                        onClose={handleClose}
-                        PaperProps={{
-                          style: {
-                            maxHeight: ITEM_HEIGHT * 4.5,
-                            width: "20ch",
-                          },
-                        }}
-                      >
-                        {options.map((option) => (
-                          <MenuItem
-                            key={option}
-                            selected={option === "Edit Post"}
-                            onClick={handleClose}
-                          >
-                            {option}
-                          </MenuItem>
-                        ))}
-                      </Menu>
-                    </div>
-                  </div>
-                  <p>
-                    Lorem ipsum represents a long-held tradition for designers,
-                    typographers and the like. Some people hate it and argue for
-                    its demise, but others ignore the hate as they create
-                    awesome tools to help create filler text for everyone from
-                    bacon lovers to Charlie Sheen fans.
-                  </p>
-
-                  <p>
-                    Lorem ipsum represents a long-held tradition for designers,
-                    typographers and the like. Some people hate it and argue for
-                    its demise, but others ignore the hate as they create
-                    awesome tools to help create filler text for everyone from
-                    bacon lovers to Charlie Sheen fans.
-                  </p>
-
-                  <p>
-                    <a href="#" class="link-black text-sm">
-                      <i class="far fa-thumbs-up mr-1"></i> 700
-                    </a>
-
-                    <span>
-                      <a
-                        href="#"
-                        class="link-black text-sm"
-                        style={{ marginLeft: 10 }}
-                      >
-                        <i class="far fa-comments mr-1"></i> 1000
-                      </a>
-                    </span>
-                  </p>
+                        {option}
+                      </MenuItem>
+                    ))}
+                  </Menu>
                 </div>
               </div>
+              <p>
+                Lorem ipsum represents a long-held tradition for designers,
+                typographers and the like. Some people hate it and argue for its
+                demise, but others ignore the hate as they create awesome tools
+                to help create filler text for everyone from bacon lovers to
+                Charlie Sheen fans.
+              </p>
+
+              <p>
+                Lorem ipsum represents a long-held tradition for designers,
+                typographers and the like. Some people hate it and argue for its
+                demise, but others ignore the hate as they create awesome tools
+                to help create filler text for everyone from bacon lovers to
+                Charlie Sheen fans.
+              </p>
+
+              <p>
+                <a href="#" class="link-black text-sm">
+                  <i class="far fa-thumbs-up mr-1"></i> 700
+                </a>
+
+                <span>
+                  <a
+                    href="#"
+                    class="link-black text-sm"
+                    style={{ marginLeft: 10 }}
+                  >
+                    <i class="far fa-comments mr-1"></i> 1000
+                  </a>
+                </span>
+              </p>
             </div>
           </div>
         </div>
