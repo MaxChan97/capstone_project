963ca2fdf80a4829e25ce9005ba8e6541009f9fa
shawnlimjunhe <shawnlimjunhe@gmail.com>
2021-01-26 19:51:53 +0800

    Update Reply.java

--- backend/penmeetspaper/penmeetspaper-ejb/src/java/entity/Reply.java
+++ backend/penmeetspaper/penmeetspaper-ejb/src/java/entity/Reply.java
@@ -24,56 +24,53 @@ import javax.persistence.ManyToOne;
 @Entity
 public class Reply implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  private static final long serialVersionUID = 1L;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @Column(nullable = false)
-    private String body;
+  @Column(nullable = false)
+  private String body;
 
-    @ManyToMany
-    private List<Person> likes = new ArrayList<>();
+  @ManyToMany
+  @JoinColumn(name = "likes_replies")
+  private List<Person> likes = new ArrayList<>();
 
-    @ManyToOne
-    @JoinColumn(name = "person_replies")
-    private Person author;
+  @ManyToOne
+  @JoinColumn(name = "person_replies")
+  private Person author;
 
-    @ManyToOne
-    @JoinColumn(name = "comment_replies")
-    private Comment comment;
+  public Long getId() {
+    return id;
+  }
 
-    public Long getId() {
-        return id;
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  @Override
+  public int hashCode() {
+    int hash = 0;
+    hash += (id != null ? id.hashCode() : 0);
+    return hash;
+  }
+
+  @Override
+  public boolean equals(Object object) {
+    // TODO: Warning - this method won't work in the case the id fields are not set
+    if (!(object instanceof Reply)) {
+      return false;
     }
-
-    public void setId(Long id) {
-        this.id = id;
+    Reply other = (Reply) object;
+    if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+      return false;
     }
+    return true;
+  }
 
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Reply)) {
-            return false;
-        }
-        Reply other = (Reply) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "entity.Reply[ id=" + id + " ]";
-    }
+  @Override
+  public String toString() {
+    return "entity.Reply[ id=" + id + " ]";
+  }
 
 }
