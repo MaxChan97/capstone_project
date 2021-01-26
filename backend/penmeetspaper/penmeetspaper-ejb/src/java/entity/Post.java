11288d1a53c64acf74ca3e4e4d26b90f55dfb8bd
shawnlimjunhe <shawnlimjunhe@gmail.com>
2021-01-26 19:42:11 +0800

    Update Post.java
    tab formatting

--- backend/penmeetspaper/penmeetspaper-ejb/src/java/entity/Post.java
+++ backend/penmeetspaper/penmeetspaper-ejb/src/java/entity/Post.java
@@ -24,110 +24,111 @@ import javax.persistence.OneToMany;
 @Entity
 public class Post implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  private static final long serialVersionUID = 1L;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    private String title;
+  private String title;
 
-    private String body;
+  private String body;
 
-    // TODO: add multiplicity 
-    private Person author;
+  @ManyToOne
+  @JoinColumn(name = "person_posts")
+  private Person author;
 
-    @ManyToMany
-    @JoinColumn(name = "likes")
-    private List<Person> likes = new ArrayList<>();
+  @ManyToMany
+  @JoinColumn(name = "likes")
+  private List<Person> likes = new ArrayList<>();
 
-    @ManyToOne
-    @JoinColumn(name = "postCommunity")
-    private Community postCommunity;
+  @ManyToOne
+  @JoinColumn(name = "postCommunity")
+  private Community postCommunity;
 
-    // unidirectional
-    @OneToMany
-    @JoinColumn(name = "postComments")
-    private List<Comment> comments = new ArrayList<>();
+  // unidirectional
+  @OneToMany
+  @JoinColumn(name = "postComments")
+  private List<Comment> comments = new ArrayList<>();
 
-    public Long getId() {
-        return id;
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getBody() {
+    return body;
+  }
+
+  public void setBody(String body) {
+    this.body = body;
+  }
+
+  public Person getAuthor() {
+    return author;
+  }
+
+  public void setAuthor(Person author) {
+    this.author = author;
+  }
+
+  public List<Person> getLikes() {
+    return likes;
+  }
+
+  public void setLikes(List<Person> likes) {
+    this.likes = likes;
+  }
+
+  public Community getPostCommunity() {
+    return postCommunity;
+  }
+
+  public void setPostCommunity(Community postCommunity) {
+    this.postCommunity = postCommunity;
+  }
+
+  public List<Comment> getComments() {
+    return comments;
+  }
+
+  public void setComments(List<Comment> comments) {
+    this.comments = comments;
+  }
+
+  @Override
+  public int hashCode() {
+    int hash = 0;
+    hash += (id != null ? id.hashCode() : 0);
+    return hash;
+  }
+
+  @Override
+  public boolean equals(Object object) {
+    // TODO: Warning - this method won't work in the case the id fields are not set
+    if (!(object instanceof Post)) {
+      return false;
     }
-
-    public void setId(Long id) {
-        this.id = id;
+    Post other = (Post) object;
+    if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+      return false;
     }
+    return true;
+  }
 
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
-
-    public Person getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(Person author) {
-        this.author = author;
-    }
-
-    public List<Person> getLikes() {
-        return likes;
-    }
-
-    public void setLikes(List<Person> likes) {
-        this.likes = likes;
-    }
-
-    public Community getPostCommunity() {
-        return postCommunity;
-    }
-
-    public void setPostCommunity(Community postCommunity) {
-        this.postCommunity = postCommunity;
-    }
-
-    public List<Comment> getComments() {
-        return comments;
-    }
-
-    public void setComments(List<Comment> comments) {
-        this.comments = comments;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Post)) {
-            return false;
-        }
-        Post other = (Post) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "entity.Post[ id=" + id + " ]";
-    }
+  @Override
+  public String toString() {
+    return "entity.Post[ id=" + id + " ]";
+  }
 
 }
