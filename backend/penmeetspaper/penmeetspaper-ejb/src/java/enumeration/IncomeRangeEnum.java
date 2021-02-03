7fb03292ff0f00ab9983da59b4eb9ac1d702a9bf
shawnlimjunhe <shawnlimjunhe@gmail.com>
2021-02-03 14:46:18 +0800

    Create IncomeRangeEnum.java

---
+++ backend/penmeetspaper/penmeetspaper-ejb/src/java/enumeration/IncomeRangeEnum.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package enumeration;
+
+/**
+ *
+ * @author Shawn
+ */
+public enum IncomeRangeEnum {
+  NOT_EARNING,
+  LOW,
+  MIDDLE_LOW,
+  MIDDLE,
+  MIDDLE_HIGH,
+  HIGH,
+  CRA
+}
