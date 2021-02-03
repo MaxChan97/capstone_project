74f320dae8d4b8ee9db8b52c39eca7ef5cf6b939
shawnlimjunhe <shawnlimjunhe@gmail.com>
2021-02-03 14:33:54 +0800

    Create AdminLogsTypeEnum.java

---
+++ backend/penmeetspaper/penmeetspaper-ejb/src/java/enumeration/AdminLogsTypeEnum.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package enumeration;
+
+/**
+ *
+ * @author Shawn
+ */
+public enum AdminLogsTypeEnum {
+  ResolveReport,
+  VoidReport,
+  BanUser,
+  UnbanUser,
+  DeletePost,
+  DeleteComment,
+  DeleteReply,
+  DeleteVideo,
+  DeleteCommunity,
+  SendSystemWideNotification,
+  SendPrivateNotification,
+  AddAdvertisement,
+  DeleteAdvertisement
+}
