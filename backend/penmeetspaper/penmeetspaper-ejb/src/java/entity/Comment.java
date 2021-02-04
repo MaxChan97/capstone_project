c56e109f0cc6fbea93db2ac7d3e3ca15a4a6c9d1
shawnlimjunhe <shawnlimjunhe@gmail.com>
2021-02-04 15:47:32 +0800

    Update Comment.java

--- backend/penmeetspaper/penmeetspaper-ejb/src/java/entity/Comment.java
+++ backend/penmeetspaper/penmeetspaper-ejb/src/java/entity/Comment.java
@@ -8,6 +8,7 @@ package entity;
 import entity.personEntities.Person;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -18,6 +19,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 /**
  *
@@ -26,92 +29,103 @@ import javax.persistence.OneToMany;
 @Entity
 public class Comment implements Serializable {
 
-  private static final long serialVersionUID = 1L;
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-  @Column(nullable = false)
-  private String body;
+    @Column(nullable = false)
+    private String body;
 
-  // Undirectional
-  @ManyToMany
-  @JoinColumn(name = "likes_comment")
-  private List<Person> likes = new ArrayList<>();
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date datePosted;
 
-  // Undirectional
-  @ManyToOne
-  @JoinColumn(name = "person_comment")
-  private Person author;
+    // Undirectional
+    @ManyToMany
+    @JoinColumn(name = "likes_comment")
+    private List<Person> likes = new ArrayList<>();
 
-  // Undirectional
-  @OneToMany
-  @JoinColumn(name = "comment_replies")
-  private List<Reply> replies = new ArrayList<>();
+    // Undirectional
+    @ManyToOne
+    @JoinColumn(name = "person_comment")
+    private Person author;
 
-  public Long getId() {
-    return id;
-  }
+    // Undirectional
+    @OneToMany
+    @JoinColumn(name = "comment_replies")
+    private List<Reply> replies = new ArrayList<>();
 
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getBody() {
-    return body;
-  }
-
-  public void setBody(String body) {
-    this.body = body;
-  }
-
-  public List<Person> getLikes() {
-    return likes;
-  }
-
-  public void setLikes(List<Person> likes) {
-    this.likes = likes;
-  }
-
-  public Person getAuthor() {
-    return author;
-  }
-
-  public void setAuthor(Person author) {
-    this.author = author;
-  }
-
-  public List<Reply> getReplies() {
-    return replies;
-  }
-
-  public void setReplies(List<Reply> replies) {
-    this.replies = replies;
-  }
-
-  @Override
-  public int hashCode() {
-    int hash = 0;
-    hash += (id != null ? id.hashCode() : 0);
-    return hash;
-  }
-
-  @Override
-  public boolean equals(Object object) {
-    // TODO: Warning - this method won't work in the case the id fields are not set
-    if (!(object instanceof Comment)) {
-      return false;
+    public Long getId() {
+        return id;
     }
-    Comment other = (Comment) object;
-    if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-      return false;
-    }
-    return true;
-  }
 
-  @Override
-  public String toString() {
-    return "entity.Comment[ id=" + id + " ]";
-  }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getDatePosted() {
+        return datePosted;
+    }
+
+    public void setDatePosted(Date datePosted) {
+        this.datePosted = datePosted;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public List<Person> getLikes() {
+        return likes;
+    }
+
+    public void setLikes(List<Person> likes) {
+        this.likes = likes;
+    }
+
+    public Person getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Person author) {
+        this.author = author;
+    }
+
+    public List<Reply> getReplies() {
+        return replies;
+    }
+
+    public void setReplies(List<Reply> replies) {
+        this.replies = replies;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Comment)) {
+            return false;
+        }
+        Comment other = (Comment) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Comment[ id=" + id + " ]";
+    }
 
 }
