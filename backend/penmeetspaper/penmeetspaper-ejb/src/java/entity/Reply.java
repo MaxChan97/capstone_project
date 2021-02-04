62f7478fce3dcdf2501e3ba5efbfa6419f03856a
shawnlimjunhe <shawnlimjunhe@gmail.com>
2021-02-04 15:48:12 +0800

    Update Reply.java

--- backend/penmeetspaper/penmeetspaper-ejb/src/java/entity/Reply.java
+++ backend/penmeetspaper/penmeetspaper-ejb/src/java/entity/Reply.java
@@ -8,6 +8,7 @@ package entity;
 import entity.personEntities.Person;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -17,6 +18,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 /**
  *
@@ -25,53 +28,84 @@ import javax.persistence.ManyToOne;
 @Entity
 public class Reply implements Serializable {
 
-  private static final long serialVersionUID = 1L;
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-  @Column(nullable = false)
-  private String body;
+    @Column(nullable = false)
+    private String body;
 
-  @ManyToMany
-  @JoinColumn(name = "likes_replies")
-  private List<Person> likes = new ArrayList<>();
+    @ManyToMany
+    @JoinColumn(name = "likes_replies")
+    private List<Person> likes = new ArrayList<>();
 
-  @ManyToOne
-  @JoinColumn(name = "person_replies")
-  private Person author;
+    @ManyToOne
+    @JoinColumn(name = "person_replies")
+    private Person author;
 
-  public Long getId() {
-    return id;
-  }
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date datePosted;
 
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  @Override
-  public int hashCode() {
-    int hash = 0;
-    hash += (id != null ? id.hashCode() : 0);
-    return hash;
-  }
-
-  @Override
-  public boolean equals(Object object) {
-    // TODO: Warning - this method won't work in the case the id fields are not set
-    if (!(object instanceof Reply)) {
-      return false;
+    public Long getId() {
+        return id;
     }
-    Reply other = (Reply) object;
-    if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-      return false;
-    }
-    return true;
-  }
 
-  @Override
-  public String toString() {
-    return "entity.Reply[ id=" + id + " ]";
-  }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public List<Person> getLikes() {
+        return likes;
+    }
+
+    public void setLikes(List<Person> likes) {
+        this.likes = likes;
+    }
+
+    public Person getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Person author) {
+        this.author = author;
+    }
+
+    public Date getDatePosted() {
+        return datePosted;
+    }
+
+    public void setDatePosted(Date datePosted) {
+        this.datePosted = datePosted;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Reply)) {
+            return false;
+        }
+        Reply other = (Reply) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Reply[ id=" + id + " ]";
+    }
 
 }
