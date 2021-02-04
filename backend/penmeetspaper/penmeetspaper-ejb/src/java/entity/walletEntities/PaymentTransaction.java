1af5a7d8efe5cd48b66257e58da5843e08cb94f3
shawnlimjunhe <shawnlimjunhe@gmail.com>
2021-02-04 10:42:52 +0800

    Create PaymentTransaction.java

---
+++ backend/penmeetspaper/penmeetspaper-ejb/src/java/entity/walletEntities/PaymentTransaction.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity.walletEntities;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author Shawn
+ */
+@Entity
+public class PaymentTransaction implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private double amount;
+
+    @Column(nullable = false)
+    private boolean isPaidToUser;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date transactionDate;
+
+    @Column(nullable = false)
+    private String messageBody;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public boolean isIsPaidToUser() {
+        return isPaidToUser;
+    }
+
+    public void setIsPaidToUser(boolean isPaidToUser) {
+        this.isPaidToUser = isPaidToUser;
+    }
+
+    public Date getTransactionDate() {
+        return transactionDate;
+    }
+
+    public void setTransactionDate(Date transactionDate) {
+        this.transactionDate = transactionDate;
+    }
+
+    public String getMessageBody() {
+        return messageBody;
+    }
+
+    public void setMessageBody(String messageBody) {
+        this.messageBody = messageBody;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof PaymentTransaction)) {
+            return false;
+        }
+        PaymentTransaction other = (PaymentTransaction) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.walletEntities.PaymentTransaction[ id=" + id + " ]";
+    }
+
+}
