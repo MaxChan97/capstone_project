8dd58965ffcb70e640062c55804eb82edcb373b3
shawnlimjunhe <shawnlimjunhe@gmail.com>
2021-02-04 15:53:13 +0800

    Update Administrator.java

--- backend/penmeetspaper/penmeetspaper-ejb/src/java/entity/adminEntities/Administrator.java
+++ backend/penmeetspaper/penmeetspaper-ejb/src/java/entity/adminEntities/Administrator.java
@@ -50,6 +50,46 @@ public class Administrator implements Serializable {
         this.id = id;
     }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public boolean isIsDeactivated() {
+        return isDeactivated;
+    }
+
+    public void setIsDeactivated(boolean isDeactivated) {
+        this.isDeactivated = isDeactivated;
+    }
+
     @Override
     public int hashCode() {
         int hash = 0;
@@ -74,5 +114,4 @@ public class Administrator implements Serializable {
     public String toString() {
         return "entity.adminEntities.Administrator[ id=" + id + " ]";
     }
-
 }
