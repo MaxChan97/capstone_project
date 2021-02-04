577d4de9a9c6daa407528154ac0ef2a0baadb198
shawnlimjunhe <shawnlimjunhe@gmail.com>
2021-02-04 13:24:33 +0800

    Create File.java

---
+++ backend/penmeetspaper/penmeetspaper-ejb/src/java/entity/messagingEntities/File.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity.messagingEntities;
+
+import enumeration.FileTypeEnum;
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Shawn
+ */
+@Entity
+public class File implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private boolean isVideo;
+
+    @Column(nullable = false)
+    private byte payload;
+
+    @Enumerated(EnumType.STRING)
+    private FileTypeEnum fileType;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isIsVideo() {
+        return isVideo;
+    }
+
+    public void setIsVideo(boolean isVideo) {
+        this.isVideo = isVideo;
+    }
+
+    public byte getPayload() {
+        return payload;
+    }
+
+    public void setPayload(byte payload) {
+        this.payload = payload;
+    }
+
+    public FileTypeEnum getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(FileTypeEnum fileType) {
+        this.fileType = fileType;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof File)) {
+            return false;
+        }
+        File other = (File) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.messagingEntities.File[ id=" + id + " ]";
+    }
+
+}
