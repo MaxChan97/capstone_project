61450fd6b1aa48664a4585b5d49d727a89d2340b
shawnlimjunhe <shawnlimjunhe@gmail.com>
2021-02-04 12:49:41 +0800

    Update Stream.java

--- backend/penmeetspaper/penmeetspaper-ejb/src/java/entity/streamingEntities/Stream.java
+++ backend/penmeetspaper/penmeetspaper-ejb/src/java/entity/streamingEntities/Stream.java
@@ -27,122 +27,144 @@ import javax.persistence.TemporalType;
 @Entity
 public class Stream implements Serializable {
 
-  private static final long serialVersionUID = 1L;
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-  @Column(nullable = false)
-  private String title;
+    @Column(nullable = false)
+    private String name;
 
-  @Column(nullable = false)
-  private boolean isPaid;
+    @Column(nullable = false)
+    private String description;
 
-  @Column(nullable = false)
-  private int viewerCount;
+    @Column(nullable = false)
+    private boolean isPaid;
 
-  @Temporal(TemporalType.TIMESTAMP)
-  private Date date;
+    @Column(nullable = false)
+    private int viewerCount;
 
-  // unidirectional
-  @OneToOne
-  private LiveChat liveChat;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date date;
 
-  // birectional
-  @OneToOne(mappedBy = "streamStreaming")
-  private Person streamer;
+    @Column(nullable = false)
+    private boolean hasEnded;
 
-  // bidirectional
-  @OneToMany(mappedBy = "streamViewing")
-  private List<Person> viewers = new ArrayList<>();
+    // unidirectional
+    @OneToOne
+    private LiveChat liveChat;
 
-  public Long getId() {
-    return id;
-  }
+    // birectional
+    @OneToOne(mappedBy = "streamStreaming")
+    private Person streamer;
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+    // bidirectional
+    @OneToMany(mappedBy = "streamViewing")
+    private List<Person> viewers = new ArrayList<>();
 
-  public String getTitle() {
-    return title;
-  }
-
-  public void setTitle(String title) {
-    this.title = title;
-  }
-
-  public boolean isIsPaid() {
-    return isPaid;
-  }
-
-  public void setIsPaid(boolean isPaid) {
-    this.isPaid = isPaid;
-  }
-
-  public int getViewerCount() {
-    return viewerCount;
-  }
-
-  public void setViewerCount(int viewerCount) {
-    this.viewerCount = viewerCount;
-  }
-
-  public Date getDate() {
-    return date;
-  }
-
-  public void setDate(Date date) {
-    this.date = date;
-  }
-
-  public LiveChat getLiveChat() {
-    return liveChat;
-  }
-
-  public void setLiveChat(LiveChat liveChat) {
-    this.liveChat = liveChat;
-  }
-
-  public Person getStreamer() {
-    return streamer;
-  }
-
-  public void setStreamer(Person streamer) {
-    this.streamer = streamer;
-  }
-
-  public List<Person> getViewers() {
-    return viewers;
-  }
-
-  public void setViewers(List<Person> viewers) {
-    this.viewers = viewers;
-  }
-
-  @Override
-  public int hashCode() {
-    int hash = 0;
-    hash += (id != null ? id.hashCode() : 0);
-    return hash;
-  }
-
-  @Override
-  public boolean equals(Object object) {
-    // TODO: Warning - this method won't work in the case the id fields are not set
-    if (!(object instanceof Stream)) {
-      return false;
+    public Long getId() {
+        return id;
     }
-    Stream other = (Stream) object;
-    if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-      return false;
-    }
-    return true;
-  }
 
-  @Override
-  public String toString() {
-    return "entity.Stream[ id=" + id + " ]";
-  }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isIsPaid() {
+        return isPaid;
+    }
+
+    public void setIsPaid(boolean isPaid) {
+        this.isPaid = isPaid;
+    }
+
+    public int getViewerCount() {
+        return viewerCount;
+    }
+
+    public void setViewerCount(int viewerCount) {
+        this.viewerCount = viewerCount;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public boolean isHasEnded() {
+        return hasEnded;
+    }
+
+    public void setHasEnded(boolean hasEnded) {
+        this.hasEnded = hasEnded;
+    }
+
+    public LiveChat getLiveChat() {
+        return liveChat;
+    }
+
+    public void setLiveChat(LiveChat liveChat) {
+        this.liveChat = liveChat;
+    }
+
+    public Person getStreamer() {
+        return streamer;
+    }
+
+    public void setStreamer(Person streamer) {
+        this.streamer = streamer;
+    }
+
+    public List<Person> getViewers() {
+        return viewers;
+    }
+
+    public void setViewers(List<Person> viewers) {
+        this.viewers = viewers;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Stream)) {
+            return false;
+        }
+        Stream other = (Stream) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Stream[ id=" + id + " ]";
+    }
 
 }
