a8d1cac8c8bbaae9140aa429c13fa7996f01d829
shawnlimjunhe <shawnlimjunhe@gmail.com>
2021-02-04 12:44:43 +0800

    Create Badge.java

---
+++ backend/penmeetspaper/penmeetspaper-ejb/src/java/entity/Badge.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import enumeration.BadgeTypeEnum;
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Shawn
+ */
+@Entity
+public class Badge implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String displayName;
+
+    @Enumerated(EnumType.STRING)
+    private BadgeTypeEnum badgeType;
+
+    @Column(nullable = false)
+    private int valueRequired;
+
+    @Column(nullable = false)
+    private String image;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public BadgeTypeEnum getBadgeType() {
+        return badgeType;
+    }
+
+    public void setBadgeType(BadgeTypeEnum badgeType) {
+        this.badgeType = badgeType;
+    }
+
+    public int getValueRequired() {
+        return valueRequired;
+    }
+
+    public void setValueRequired(int valueRequired) {
+        this.valueRequired = valueRequired;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Badge)) {
+            return false;
+        }
+        Badge other = (Badge) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Badge[ id=" + id + " ]";
+    }
+
+}
