f00d7e8e2beb72f97261372c25f3aa9f09cd36ec
shawnlimjunhe <shawnlimjunhe@gmail.com>
2021-02-21 19:30:17 +0800

    CRUD Methods for ReplySB

--- backend/penmeetspaper/penmeetspaper-ejb/src/java/session/ReplySessionBeanLocal.java
+++ backend/penmeetspaper/penmeetspaper-ejb/src/java/session/ReplySessionBeanLocal.java
@@ -5,6 +5,9 @@
  */
 package session;
 
+import entity.Reply;
+import exception.NoResultException;
+import exception.NotValidException;
 import javax.ejb.Local;
 
 /**
@@ -13,5 +16,28 @@ import javax.ejb.Local;
  */
 @Local
 public interface ReplySessionBeanLocal {
+
+    public final static String MISSING_REPLY = "Missing reply parameter";
+    public final static String INVALID_CREDENTIALS = "Incorrect credentials to modify Comment";
+
+    public final static String MISSING_PERSON_ID = "Missing person id";
+    public final static String MISSING_COMMENT_ID = "Missing comment id";
+    public final static String MISSING_REPLY_ID = "Missing reply id";
+
+    public final static String CANNOT_FIND_PERSON = "Cannot find person";
+    public final static String CANNOT_FIND_COMMENT = "Cannot find comment";
+    public final static String CANNOT_FIND_REPLY = "Cannot find reply";
+
+    public Reply getReply(Long replyId) throws NoResultException, NotValidException;
+
+    public void createReplyForComment(Long personId, Long commentId, Reply reply) throws NoResultException, NotValidException;
+
+    public void updateReply(Reply reply, Long personId) throws NoResultException, NotValidException;
+
+    public void deleteReply(Long replyId, Long personId) throws NoResultException, NotValidException;
+
+    public void likeReply(Long replyId, Long personId) throws NoResultException, NotValidException;
+
+    public void unlikeReply(Long replyId, Long personId) throws NoResultException, NotValidException;
 
 }
