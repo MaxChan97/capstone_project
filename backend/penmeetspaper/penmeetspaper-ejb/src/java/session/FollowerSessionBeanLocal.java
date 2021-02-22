9a1b28c89e2204511fb9ce225b98ef6d4e460681
shawnlimjunhe <shawnlimjunhe@gmail.com>
2021-02-22 16:14:53 +0800

    Created follow resources

---
+++ backend/penmeetspaper/penmeetspaper-ejb/src/java/session/FollowerSessionBeanLocal.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package session;
+
+import javax.ejb.Local;
+
+/**
+ *
+ * @author shawn
+ */
+@Local
+public interface FollowerSessionBeanLocal {
+
+    public final static String MULTIPLE_FOLLOW_FOUND = "Multiple Follow entities found";
+    public final static String FOLLOW_ALREADY_EXISTS = "Follow Entity already exists";
+
+    public final static String MISSING_PERSON_ID = "Missing person ID";
+
+    public final static String CANNOT_FIND_PERSON = "Could not find person";
+    public final static String CANNOT_FIND_FOLLOW = "Could not find follow Entity that you were looking for";
+}
