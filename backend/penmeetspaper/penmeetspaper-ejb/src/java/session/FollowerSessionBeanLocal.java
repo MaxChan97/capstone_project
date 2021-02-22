26e00782648620e099cd70ffd2e8d1223b835ea1
shawnlimjunhe <shawnlimjunhe@gmail.com>
2021-02-22 16:26:25 +0800

    Implemented Follow endpoints

--- backend/penmeetspaper/penmeetspaper-ejb/src/java/session/FollowerSessionBeanLocal.java
+++ backend/penmeetspaper/penmeetspaper-ejb/src/java/session/FollowerSessionBeanLocal.java
@@ -5,6 +5,8 @@
  */
 package session;
 
+import exception.NoResultException;
+import exception.NotValidException;
 import javax.ejb.Local;
 
 /**
@@ -21,4 +23,8 @@ public interface FollowerSessionBeanLocal {
 
     public final static String CANNOT_FIND_PERSON = "Could not find person";
     public final static String CANNOT_FIND_FOLLOW = "Could not find follow Entity that you were looking for";
+
+    public void followPerson(Long followerId, Long publisherId) throws NoResultException, NotValidException;
+
+    public void unfollowPerson(Long followerId, Long publisherId) throws NoResultException, NotValidException;
 }
--- backend/penmeetspaper/penmeetspaper-war/src/java/webservices/restful/FollowResource.java
+++ backend/penmeetspaper/penmeetspaper-war/src/java/webservices/restful/FollowResource.java
@@ -5,7 +5,19 @@
  */
 package webservices.restful;
 
+import exception.NoResultException;
+import exception.NotValidException;
+import javax.ejb.EJB;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import session.FollowerSessionBeanLocal;
 
 /**
  *
@@ -13,5 +25,45 @@ import javax.ws.rs.Path;
  */
 @Path("follow")
 public class FollowResource {
+
+    @EJB
+    private FollowerSessionBeanLocal followSBLocal;
+
+    private Response buildError(Exception e, int statusCode) {
+        JsonObject exception = Json.createObjectBuilder()
+                .add("error", e.getMessage())
+                .build();
+
+        return Response.status(statusCode).entity(exception)
+                .type(MediaType.APPLICATION_JSON).build();
+    }
+
+    @POST
+    @Path("/{followerId}/{publisherId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response followPerson(@PathParam("followerId") Long followerId, @PathParam("publisherId") Long publisherId) {
+        try {
+
+            followSBLocal.followPerson(followerId, publisherId);
+            return Response.status(204).build();
+
+        } catch (NoResultException | NotValidException e) {
+            return buildError(e, 400);
+        }
+    }
+
+    @DELETE
+    @Path("/{followerId}/{publisherId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response unfollowPerson(@PathParam("followerId") Long followerId, @PathParam("publisherId") Long publisherId) {
+        try {
+
+            followSBLocal.unfollowPerson(followerId, publisherId);
+            return Response.status(204).build();
+
+        } catch (NoResultException | NotValidException e) {
+            return buildError(e, 400);
+        }
+    }
 
 }
