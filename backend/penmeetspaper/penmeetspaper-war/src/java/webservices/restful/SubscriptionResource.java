b7e263693dc9e8c278c0b40c48b6b6234b5f1e76
shawnlimjunhe <shawnlimjunhe@gmail.com>
2021-02-22 18:27:08 +0800

    Added endpoints for subscriptions

--- backend/penmeetspaper/penmeetspaper-war/src/java/webservices/restful/SubscriptionResource.java
+++ backend/penmeetspaper/penmeetspaper-war/src/java/webservices/restful/SubscriptionResource.java
@@ -5,8 +5,18 @@
  */
 package webservices.restful;
 
+import exception.NoResultException;
+import exception.NotValidException;
 import javax.ejb.EJB;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import session.SubscriptionSessionBeanLocal;
 
 /**
@@ -18,5 +28,42 @@ public class SubscriptionResource {
 
     @EJB
     private SubscriptionSessionBeanLocal subscriptionSB;
+
+    private Response buildError(Exception e, int statusCode) {
+        JsonObject exception = Json.createObjectBuilder()
+                .add("error", e.getMessage())
+                .build();
+
+        return Response.status(statusCode).entity(exception)
+                .type(MediaType.APPLICATION_JSON).build();
+    }
+
+    @POST
+    @Path("/subscriber/{subscriberId}/publisher/{publisherId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response subscribeToPerson(@PathParam("subscriberId") Long subscriberId, @PathParam("publisherId") Long publisherId) {
+        try {
+
+            subscriptionSB.subscribeToPerson(subscriberId, publisherId);
+            return Response.status(204).build();
+
+        } catch (NoResultException | NotValidException e) {
+            return buildError(e, 400);
+        }
+    }
+
+    @DELETE
+    @Path("/subscriber/{subscriberId}/publisher/{publisherId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response unsubscribeToPerson(@PathParam("subscriberId") Long subscriberId, @PathParam("publisherId") Long publisherId) {
+        try {
+
+            subscriptionSB.unsubscribeToPerson(subscriberId, publisherId);
+            return Response.status(204).build();
+
+        } catch (NoResultException | NotValidException e) {
+            return buildError(e, 400);
+        }
+    }
 
 }
