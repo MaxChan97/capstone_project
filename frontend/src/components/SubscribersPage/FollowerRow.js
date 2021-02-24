54e167ef9b6b537cebe7960b314d58c4d0a3174d
Megan Chan <e0316017@u.nus.edu>
2021-02-24 13:06:35 +0800

    Subscription Page Frontend fix

--- frontend/src/components/SubscribersPage/FollowerRow.js
+++ frontend/src/components/SubscribersPage/FollowerRow.js
@@ -1,18 +1,19 @@
 import React from "react";
 import defaultDP from "../../assets/Default Dp logo.svg";
 
-export default function UserRow({ data }) {
+export default function FollowerRow({ follower }) {
+
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-2">
-          <p className="text-left">{data.rank}</p>
+          <p className="text-left"> {follower.name} </p>
         </div>
         <div className="col-md-3">
           <img className="img-fluid" src={defaultDP} alt="defaultDP" />
         </div>
         <div className="col-md-7">
-          <p className="text-left">{data.name}</p>
+          <p className="text-left">{follower.name}</p>
         </div>
       </div>
     </div>
--- frontend/src/components/SubscribersPage/SubscriberCard.js
+++ frontend/src/components/SubscribersPage/SubscriberCard.js
@@ -1,22 +1,31 @@
 import React from "react";
-import UserRow from "./UserRow";
+import SubscriberRow from "./SubscriberRow";
 
-export default function SubscriberCard({ data }) {
+export default function SubscriberCard({ subscriberList }) {
+
   return (
-    <div className="card card-primary">
-      <div className="card-body">
-        <p className="font-weight-bold">Subscribers</p>
-        <p className="font-weight-light">Number of subscribers:</p>
-        <ul class="list-group list-group-flush">
-          {data.map((row, index) => {
-            return (
-              <li key={index} class="list-group-item">
-                <UserRow data={row} />
-              </li>
-            );
-          })}
-        </ul>
+    subscriberList !== undefined ? (
+      <div className="card card-primary">
+        <div className="card-body">
+          <p className="font-weight-bold">Subscribers</p>
+
+          <p className="font-weight-light">
+
+            <p className="font-weight-light"> Number of subscribers: {subscriberList.length}</p>
+
+          </p>
+
+          <ul class="list-group list-group-flush">
+            {subscriberList.map((row, index) => {
+              return (
+                <li key={index} class="list-group-item">
+                  <SubscriberRow subscriber={row} />
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
-    </div>
-  );
-}
+
+    ) : (""))
+          }
