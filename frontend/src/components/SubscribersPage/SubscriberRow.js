7c3b13930ad5fa7f676c6f9e0b02ab3235a4e924
Megan Chan <e0316017@u.nus.edu>
2021-02-25 13:47:02 +0800

    Create SubscriberRow

---
+++ frontend/src/components/SubscribersPage/SubscriberRow.js
@@ -0,0 +1,20 @@
+import React from "react";
+import defaultDP from "../../assets/Default Dp logo.svg";
+
+export default function SubscriberRow({ subscriber }) {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-2">
+          <p className="text-left">{subscriber.name}</p>
+        </div>
+        <div className="col-md-3">
+          <img className="img-fluid" src={defaultDP} alt="defaultDP" />
+        </div>
+        <div className="col-md-7">
+          <p className="text-left">{subscriber.name}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
