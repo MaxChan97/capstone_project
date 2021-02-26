42d656668959304ebf1b6def957b3822173a4fe6
Megan Chan <e0316017@u.nus.edu>
2021-02-27 03:25:50 +0800

    Complete Subscriber Pg logic

--- frontend/src/components/SubscribersPage/FollowerRow.js
+++ frontend/src/components/SubscribersPage/FollowerRow.js
@@ -2,18 +2,19 @@ import React from "react";
 import defaultDP from "../../assets/Default Dp logo.svg";
 
 export default function FollowerRow({ follower }) {
+  const person = follower["follower"]
 
   return (
     <div className="container">
       <div className="row">
-        <div className="col-md-2">
-          <p className="text-left"> {follower.name} </p>
-        </div>
+        {/* <div className="col-md-2">
+          <p className="text-left"> {person.username} </p>
+        </div> */}
         <div className="col-md-3">
           <img className="img-fluid" src={defaultDP} alt="defaultDP" />
         </div>
         <div className="col-md-7">
-          <p className="text-left">{follower.name}</p>
+          <p className="text-left">{person.username}</p>
         </div>
       </div>
     </div>
