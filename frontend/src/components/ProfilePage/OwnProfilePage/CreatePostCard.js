f1acb21fe81b566cac88c59cbfae68b668e0bfbf
chongxyxyxy <56869430+chongxyxyxy@users.noreply.github.com>
2021-03-02 03:00:31 +0800

    touch up post styling

--- frontend/src/components/ProfilePage/OwnProfilePage/CreatePostCard.js
+++ frontend/src/components/ProfilePage/OwnProfilePage/CreatePostCard.js
@@ -139,6 +139,7 @@ export default function CreatePostCard({ personId, refresh, setRefresh }) {
           style={{
             minWidth: "82ch",
             maxWidth: "82ch",
+            margin: "auto",
           }}
         >
           <div className="card-body">
@@ -154,20 +155,20 @@ export default function CreatePostCard({ personId, refresh, setRefresh }) {
                     progress < 100 ? (
                       <progress value={progress} max="100" />
                     ) : (
-                      <img
-                        className="mx-auto d-block"
-                        width="300"
-                        src={fileUrl}
-                      />
-                    )
+                        <img
+                          className="mx-auto d-block"
+                          width="300"
+                          src={fileUrl}
+                        />
+                      )
                   ) : (
-                    <div>
-                      <FileTypes data={fileName.split(".")[1]}></FileTypes>
-                      <p className="text-center font-weight-bold">
-                        {fileName.split(".")[0]}
-                      </p>
-                    </div>
-                  ))}
+                      <div>
+                        <FileTypes data={fileName.split(".")[1]}></FileTypes>
+                        <p className="text-center font-weight-bold">
+                          {fileName.split(".")[0]}
+                        </p>
+                      </div>
+                    ))}
                 <TextField
                   id="standard-textarea"
                   placeholder="What's new?"
@@ -190,8 +191,8 @@ export default function CreatePostCard({ personId, refresh, setRefresh }) {
                 />
               </div>
             ) : (
-              ""
-            )}
+                ""
+              )}
 
             <div className="row">
               <div className="col-6">
@@ -246,7 +247,7 @@ export default function CreatePostCard({ personId, refresh, setRefresh }) {
             </div>
           </div>
         </div>
-      </form>
+      </form>      
     </div>
   );
 }
