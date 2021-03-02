b3d6b7eb6f34ac4915b094bdfd778aed9e3e6a38
Megan Chan <e0316017@u.nus.edu>
2021-03-02 18:51:44 +0800

    FollowerCount added, Following Pg done

--- frontend/src/components/FollowingPage/ChannelRow.js
+++ frontend/src/components/FollowingPage/ChannelRow.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import defaultDP from "../../assets/Default Dp logo.svg";
+import Api from "../../helpers/Api";
+
 
 export default function FollowingRow({ channel }) {
   console.log(channel)
   const publisher = channel["publisher"]
+
+  const [followingList, setFollowingList] = useState([]);
+
+  // Get total num of followers for each channel
+  function loadData(publisher) {
+
+    Api.getFollowing(publisher["id"])
+      .done((data) => {
+        setFollowingList(data);
+        console.log(publisher)
+      })
+      .fail((xhr, status, error) => {
+        if (xhr, status, error === "Cannot find publisher") {
+          alert.show("publisher not found");
+        } else if (xhr, status, error === "Missing publisher id") {
+          alert.show("The publisher ID is missing");
+        }
+      });
+  }
 
   return (
     <div className="container">
@@ -15,9 +36,11 @@ export default function FollowingRow({ channel }) {
           <img className="img-fluid" src={publisher.profilePicture} alt="defaultDP" />
         </div>
         <div className="col-md-7">
-          <p className="text-left">{publisher.username}</p>
+        <dl>
+          <dt className="text-left-bold">{publisher.username}</dt>
+          <dd className="text-left-small">{followingList.length} followers</dd>
+          </dl>
         </div>
-        // insert channel follower count here
       </div>
     </div>
   );
