77fe8a5194f8ec63660bd25a717a19ededa6ed74
singjielim <39295668+singjielim@users.noreply.github.com>
2021-03-10 10:51:05 +0800

    ban button hover working
    ban button hover working

--- frontend/src/components/CommunityPage/ManageMembersRow.js
+++ frontend/src/components/CommunityPage/ManageMembersRow.js
@@ -5,16 +5,16 @@ import ban from "../../assets/ban.svg";
 import { useAlert } from "react-alert";
 import Api from "../../helpers/Api";
 import {
-    Button,
-    Dialog,
-    DialogActions,
-    DialogContent,
-    DialogContentText,
-    DialogTitle,
-  } from "@material-ui/core";
-  import { withStyles } from "@material-ui/core/styles";
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
 
-export default function ManageMembersRow({ member , refresh, setRefresh}) {
+export default function ManageMembersRow({ member, refresh, setRefresh }) {
   const alert = useAlert();
   const { communityId } = useParams();
 
@@ -87,7 +87,10 @@ export default function ManageMembersRow({ member , refresh, setRefresh}) {
           if (xhr.responseJSON.error === "Person already banned") {
             alert.show("Person already banned");
           }
-          if (xhr.responseJSON.error === "The owner of the community cannot be banned") {
+          if (
+            xhr.responseJSON.error ===
+            "The owner of the community cannot be banned"
+          ) {
             alert.show("The owner of the community cannot be banned");
           }
         });
@@ -110,6 +113,11 @@ export default function ManageMembersRow({ member , refresh, setRefresh}) {
         <div className="col-md-3">
           <img
             className="img-fluid"
+            style ={{resizeMode: "repeat",
+            height: 40,
+            width: 40,
+            borderRadius: "50%",
+            display: "block"}}
             src={member.profilePicture}
             alt="defaultDP"
           />
@@ -118,47 +126,53 @@ export default function ManageMembersRow({ member , refresh, setRefresh}) {
           {member.username}
         </div>
         <div className="col-md-2">
-          <img
+          <button
             style={{
               height: "25px",
               width: "25px",
+              backgroundColor: "transparent",
+              borderColor: "transparent",
             }}
-            src={ban}
-            alt="banLogo"
-            onClick={banPerson}
-          />
+          >
+            <img
+              style={{
+                height: "25px",
+                width: "25px",
+              }}
+              src={ban}
+              alt="banLogo"
+              onClick={banPerson}
+            />
+          </button>
           <Dialog
-                open={confirmBanDialogOpen}
-                onClose={closeBanPersonModal}
-                aria-labelledby="confirm-delete-dialog-title"
-                aria-describedby="confirm-delete-dialog-description"
+            open={confirmBanDialogOpen}
+            onClose={closeBanPersonModal}
+            aria-labelledby="confirm-delete-dialog-title"
+            aria-describedby="confirm-delete-dialog-description"
+          >
+            <DialogTitle id="confirm-delete-dialog-title">
+              Ban {member.username}
+            </DialogTitle>
+            <DialogContent>
+              <DialogContentText id="confirm-delete-dialog-description">
+                Are you sure you want to ban user {member.username}?
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button style={{ outline: "none" }} onClick={closeBanPersonModal}>
+                Cancel
+              </Button>
+              <ColorButton
+                style={{ outline: "none" }}
+                onClick={handleBanPerson}
+                color="primary"
+                variant="contained"
+                autoFocus
               >
-                <DialogTitle id="confirm-delete-dialog-title">
-                  Ban {member.username}
-                </DialogTitle>
-                <DialogContent>
-                  <DialogContentText id="confirm-delete-dialog-description">
-                    Are you sure you want to ban user {member.username}?
-                  </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                  <Button
-                    style={{ outline: "none" }}
-                    onClick={closeBanPersonModal}
-                  >
-                    Cancel
-                  </Button>
-                  <ColorButton
-                    style={{ outline: "none" }}
-                    onClick={handleBanPerson}
-                    color="primary"
-                    variant="contained"
-                    autoFocus
-                  >
-                    Ban
-                  </ColorButton>
-                </DialogActions>
-              </Dialog>
+                Ban
+              </ColorButton>
+            </DialogActions>
+          </Dialog>
         </div>
       </div>
     </div>
