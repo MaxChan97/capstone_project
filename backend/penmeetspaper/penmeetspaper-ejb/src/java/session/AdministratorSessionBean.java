28725c4a6349af83db504420370b5a47f37b389b
shawnlimjunhe <46809501+shawnlimjunhe@users.noreply.github.com>
2021-03-14 16:18:10 +0800

    Added create Admin method

---
+++ backend/penmeetspaper/penmeetspaper-ejb/src/java/session/AdministratorSessionBean.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package session;
+
+import entity.adminEntities.Administrator;
+import exception.NotValidException;
+import java.util.Date;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author SHAWN LIM
+ */
+@Stateless
+public class AdministratorSessionBean implements AdministratorSessionBeanLocal {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    private void checkUsernameTaken(String username) throws NotValidException {
+        Query q = em.createQuery("SELECT a from Administrator a WHERE a.username =:username");
+        q.setParameter("username", username);
+        if (q.getResultList().size() > 0) {
+            throw new NotValidException(AdministratorSessionBeanLocal.USERNAME_TAKEN);
+        }
+    }
+
+    private boolean isMasterAdminCreated() {
+        Query q = em.createQuery("SELECT a Administrator a");
+        List<Administrator> adminList = q.getResultList();
+        if (adminList.isEmpty()) {
+            // Master Admin cannot be deleted;
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public Administrator createAdmin(Administrator admin) throws NotValidException {
+        if (admin == null) {
+            throw new NotValidException(AdministratorSessionBeanLocal.MISSING_PERSON);
+        }
+        if (admin.getUsername() == null) {
+            throw new NotValidException(AdministratorSessionBeanLocal.MISSING_USERNAME);
+        }
+
+        Query q;
+        q = em.createQuery("SELECT a from Administrator a WHERE a.email =:email");
+        q.setParameter("email", admin.getEmail());
+        if (q.getResultList().size() > 0) {
+            throw new NotValidException(AdministratorSessionBeanLocal.EMAIL_TAKEN);
+        }
+
+        checkUsernameTaken(admin.getUsername());
+
+        admin.setCreatedDate(new Date());
+
+        admin.setIsDeactivated(false);
+
+        if (isMasterAdminCreated()) {
+            admin.setIsMaster(false);
+        } else {
+            admin.setIsMaster(true);
+        }
+
+        em.persist(admin);
+        em.flush();
+
+        return admin;
+    }
+}
---
+++ backend/penmeetspaper/penmeetspaper-ejb/src/java/session/AdministratorSessionBeanLocal.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package session;
+
+import entity.adminEntities.Administrator;
+import exception.NotValidException;
+import javax.ejb.Local;
+
+/**
+ *
+ * @author SHAWN LIM
+ */
+@Local
+public interface AdministratorSessionBeanLocal {
+
+    public final static String MISSING_PERSON = "Missing Administrator parameter";
+    public final static String MISSING_USERNAME = "Missing Username parameter";
+    public final static String USERNAME_TAKEN = "Administrator Username already taken";
+    public final static String EMAIL_TAKEN = "Administrator Email already taken";
+
+    public Administrator createAdmin(Administrator admin) throws NotValidException;
+
+}
