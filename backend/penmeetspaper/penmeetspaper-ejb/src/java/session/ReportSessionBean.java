db6c57dd31940ad6c23e37adea7f221bdf0d9c5a
shawnlimjunhe <46809501+shawnlimjunhe@users.noreply.github.com>
2021-04-01 00:26:03 +0800

    Created ReportSB and ReportSBLocal

---
+++ backend/penmeetspaper/penmeetspaper-ejb/src/java/session/ReportSessionBean.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package session;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author SHAWN LIM
+ */
+@Stateless
+public class ReportSessionBean implements ReportSessionBeanLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
