1d2a2b1647ce75c0ff3e5fdf741cddf15e2227c2
shawnlimjunhe <46809501+shawnlimjunhe@users.noreply.github.com>
2021-04-01 20:29:33 +0800

    Create NotificationSB

---
+++ backend/penmeetspaper/penmeetspaper-ejb/src/java/session/NotificationSessionBean.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package session;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author SHAWN LIM
+ */
+@Stateless
+public class NotificationSessionBean implements NotificationSessionBeanLocal {
+
+    @PersistenceContext
+    EntityManager em;
+
+}
