e51cfc5967fa71dfe4bb8f23bb932bd7df3af005
singjielim <39295668+singjielim@users.noreply.github.com>
2021-04-16 19:08:06 +0800

    layout test
    layout test

---
+++ frontend/src/components/PaymentPage/Layout.jsx
@@ -0,0 +1,24 @@
+import Head from "next/head";
+import styled from "@emotion/styled";
+import GlobalStyles from "./prebuilt/GlobalStyles";
+
+import {loadStripe} from '@stripe/stripe-js';
+import {Elements} from '@stripe/react-stripe-js';
+
+const stripePromise = loadStripe(process.env.PUBLISHABLE_KEY);
+
+const Layout = ({ children, title }) => {
+  return (
+    <>
+      <GlobalStyles />
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Elements stripe={stripePromise}>{children}</Elements>
+    </>
+  );
+};
+
+export default Layout;
