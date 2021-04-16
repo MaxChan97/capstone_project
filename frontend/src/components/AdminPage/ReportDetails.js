4ec477f5fd2cb8f63f3888479827e890777a36f3
chongxyxyxy <56869430+chongxyxyxy@users.noreply.github.com>
2021-04-16 23:06:34 +0800

    add dialog for notifs in report details

--- frontend/src/components/AdminPage/ReportDetails.js
+++ frontend/src/components/AdminPage/ReportDetails.js
@@ -11,6 +11,13 @@ import { Link } from "react-router-dom";
 import { typography } from '@material-ui/system';
 import { useParams } from "react-router";
 import CircularProgress from '@material-ui/core/CircularProgress';
+import {
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+} from "@material-ui/core";
 
 export default function ReportDetails() {
     const dispatch = useDispatch();
@@ -31,7 +38,7 @@ export default function ReportDetails() {
         if (reportId) {
             loadData(reportId);
         }
-    }, [reportId,refresh]);
+    }, [reportId, refresh]);
 
     const [formatDate, setFormatDate] = useState();
     function changeDateFormat(r) {
@@ -40,7 +47,7 @@ export default function ReportDetails() {
         setFormatDate(changedDate);
     }
     const [data, setData] = useState(null);
-    
+
     function loadData(reportId) {
         Api.getReportById(reportId)
             .done((r) => {
@@ -52,99 +59,186 @@ export default function ReportDetails() {
             });
     }
 
-    function handleVoid(){
+    function handleVoid() {
         Api.changeReportState(data.id, "VOID", currentUser)
             .done((r) => {
                 setRefresh(!refresh);
+                openBanPersonModal()
             })
             .fail(() => {
                 //alert.show("Unable to load!");
             });
     }
 
-    function handleResolve(){
+    function handleResolve() {
         Api.changeReportState(data.id, "RESOLVED", currentUser)
             .done((r) => {
                 setRefresh(!refresh);
+                openBanPersonModal()
             })
             .fail(() => {
                 //alert.show("Unable to load!");
             });
     }
 
+    const [confirmBanDialogOpen, setConfirmBanDialogOpen] = React.useState(false);
+    const [notificationMsgReporter, setNotificationMsgReporter] = React.useState("");
+    const [notificationMsgReported, setNotificationMsgReported] = React.useState("");
+    function openBanPersonModal() {
+        setConfirmBanDialogOpen(true);
+    }
+
+    function closeBanPersonModal() {
+        setConfirmBanDialogOpen(false);
+        setNotificationMsgReporter("");
+        setNotificationMsgReported("");
+    }
+
+    function handleMsgSubmit() {
+        //send notification to user on report status
+    }
+
+    //person that created report
+    const handleReporterMsgChange = (event) => {
+        setNotificationMsgReporter(event.target.value);
+    }
+
+    //person being reported
+    const handleReportedMsgChange = (event) => {
+        setNotificationMsgReported(event.target.value);
+    }
+
     return data ? (
-        <div className="content-wrapper">
-            <div className="container mt-3" style={{ paddingTop: 20, paddingBottom: 5 }}>
+        <div>
+            <Dialog
+                open={confirmBanDialogOpen}
+                onClose={closeBanPersonModal}
+                aria-labelledby="confirm-delete-dialog-title"
+                aria-describedby="confirm-delete-dialog-description"
+                fullWidth={true}
+                maxWidth="md"
+            >
+                <DialogTitle id="confirm-delete-dialog-title">
+                    Report Status Updated {" "}
+                    <i class='far fa-check-circle' style={{ color: "#3B21CB" }}></i>
+                </DialogTitle>
+                <DialogContent style={{ overflowY: 'visible' }}>
+                    <DialogContentText id="confirm-delete-dialog-description">
+                        <i class='fas fa-exclamation-circle' style={{ color: "#3B21CB" }}></i> {" "}
+                        Craft a message to be sent to involved users to update them on the report progress.
+                        <br></br> Notifications containing the messages will be sent.
+                    </DialogContentText>
 
-                <div class="col-md-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <h3 class="card-title">
-                                <i class="far fa-folder-open"></i>
-                                {" "} Review Report: {data.reportType}
-                            </h3>
-                        </div>
+                    <p htmlFor="inputReason"> <i class='fas fa-pen'></i> {" "}Enter message for reporter: </p>
+                    <textarea
+                        type="text"
+                        id="inputReason"
+                        className="form-control"
+                        value={notificationMsgReporter}
+                        required="required"
+                        onChange={handleReporterMsgChange}
+                    />
+                    <br></br>
+                    {data.reportType != "SYSTEM_REPORT" ? (
+                        <div>
+                            <p htmlFor="inputReason">  <i class='fas fa-pen'></i> {" "}Enter message for user that is being reported: </p>
+                            <textarea
+                                type="text"
+                                id="inputReason"
+                                className="form-control"
+                                value={notificationMsgReported}
+                                required="required"
+                                onChange={handleReportedMsgChange}
+                            />
+                        </div>) : ("")}
+                </DialogContent>
+                <DialogActions>
+                    <Button style={{ outline: "none" }} onClick={closeBanPersonModal}>
+                        Cancel
+                    </Button>
+                    <ColorButton
+                        style={{ outline: "none" }}
+                        onClick={handleMsgSubmit}
+                        color="primary"
+                        variant="contained"
+                    >
+                        Confirm
+                    </ColorButton>
+                </DialogActions>
+            </Dialog>
+            <div className="content-wrapper">
+                <div className="container mt-3" style={{ paddingTop: 20, paddingBottom: 5 }}>
 
-                        <div class="card-body">
-                            <dl class="row">
-                                <dt class="col-sm-2">  Report id:</dt>
-                                <dd class="col-sm-10">{data.id}</dd>
-                                <dt class="col-sm-2"> Reported by: </dt>
-                                <dd class="col-sm-10"><Link to={"/profile/" + data.reporter.id} style={{ color: "#3B21CB", }}>{data.reporter.username}</Link></dd>
-                                <dt class="col-sm-2">Reported on: </dt>
-                                <dd class="col-sm-10">{moment(formatDate).format("DD/MM/YYYY hh:mm:ss a")}</dd>
-                                <dt class="col-sm-2">Report status:</dt>
-                                <dd class="col-sm-10">{data.reportState}</dd>
-                                {data.reportType != "SYSTEM_REPORT" ? (
-                                    <dt class="col-sm-2">Reason:</dt>
-                                ) : (<dt class="col-sm-2">Reported feature:</dt>)}
-                                <dd class="col-sm-10">{data.category}</dd>
-                                <dt class="col-sm-2">User message: </dt>
-                                <dd class="col-sm-10">{data.messageBody}</dd>
-                                {data.reportType != "SYSTEM_REPORT" ? (
-                                    <dd class="col-sm-9"> <Link to={data.reportContentId} style={{ color: "#3B21CB", }}><i class="fas fa-lightbulb"></i> {" "}View Reported Content/User</Link>
-                                    </dd>
-                                ) : ("")}
-                            </dl>
-                            {data.reportState == "PENDING" ? (
-                               
-                            <div style={{ textAlign: "right" }}>
-                                <ColorButton
-                                    style={{
-                                        height: "30px",
-                                        width: "100px",
-                                        outline: "none",
-                                        marginRight: "3%",
-                                    }}
-                                    variant="contained"
-                                    color="primary"
-                                    type="button"
-                                    onClick={handleVoid}
-                                >
-                                    Void
-                                </ColorButton>
-                                <ColorButton
-                                    style={{
-                                        height: "30px",
-                                        width: "100px",
-                                        outline: "none",
-                                        marginRight: "3%",
-                                        backgroundColor: "#EA3F79",
-                                    }}
-                                    variant="contained"
-                                    color="primary"
-                                    type="button"
-                                    onClick={handleResolve}
-                                >
-                                    Resolve
-                                </ColorButton>
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">
+                                    <i class="far fa-folder-open"></i>
+                                    {" "} Review Report: {data.reportType}
+                                </h3>
                             </div>
-                             ) : ("")}
+
+                            <div class="card-body">
+                                <dl class="row">
+                                    <dt class="col-sm-2">  Report id:</dt>
+                                    <dd class="col-sm-10">{data.id}</dd>
+                                    <dt class="col-sm-2"> Reported by: </dt>
+                                    <dd class="col-sm-10"><Link to={"/profile/" + data.reporter.id} style={{ color: "#3B21CB", }}>{data.reporter.username}</Link></dd>
+                                    <dt class="col-sm-2">Reported on: </dt>
+                                    <dd class="col-sm-10">{moment(formatDate).format("DD/MM/YYYY hh:mm:ss a")}</dd>
+                                    <dt class="col-sm-2">Report status:</dt>
+                                    <dd class="col-sm-10">{data.reportState}</dd>
+                                    {data.reportType != "SYSTEM_REPORT" ? (
+                                        <dt class="col-sm-2">Reason:</dt>
+                                    ) : (<dt class="col-sm-2">Reported feature:</dt>)}
+                                    <dd class="col-sm-10">{data.category}</dd>
+                                    <dt class="col-sm-2">User message: </dt>
+                                    <dd class="col-sm-10">{data.messageBody}</dd>
+                                    {data.reportType != "SYSTEM_REPORT" ? (
+                                        <dd class="col-sm-9"> <Link to={data.reportContentId} style={{ color: "#3B21CB", }}><i class="fas fa-lightbulb"></i> {" "}View Reported Content/User</Link>
+                                        </dd>
+                                    ) : ("")}
+                                </dl>
+                                {data.reportState == "PENDING" ? (
+
+                                    <div style={{ textAlign: "right" }}>
+                                        <ColorButton
+                                            style={{
+                                                height: "30px",
+                                                width: "100px",
+                                                outline: "none",
+                                                marginRight: "3%",
+                                            }}
+                                            variant="contained"
+                                            color="primary"
+                                            type="button"
+                                            onClick={handleVoid}
+                                        >
+                                            Void
+                                        </ColorButton>
+                                        <ColorButton
+                                            style={{
+                                                height: "30px",
+                                                width: "100px",
+                                                outline: "none",
+                                                marginRight: "3%",
+                                                backgroundColor: "#EA3F79",
+                                            }}
+                                            variant="contained"
+                                            color="primary"
+                                            type="button"
+                                            onClick={handleResolve}
+                                        >
+                                            Resolve
+                                        </ColorButton>
+                                    </div>
+                                ) : ("")}
+                            </div>
+
                         </div>
-
                     </div>
-                </div>
 
+                </div>
             </div>
         </div>
     ) : (
